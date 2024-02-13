bc7dea52e0dc54b3a8ebd921e6b0ff139c67d105
Isines1611 <isines16@gmail.com>
2024-02-13 18:20:48 +0100

    Img naruto

--- converter.py
+++ converter.py
@@ -2,13 +2,15 @@ import csv
 import json
 
 # Chemin vers le fichier CSV
-csv_file = 'OPdle.csv'
+csv_op = 'OPdle.csv'
+csv_naruto = 'Narutodle.csv'
 
 # Liste pour stocker les personnages
 characters = []
+n_characters = []
 
 # Lire le fichier CSV et ajouter les personnages à la liste
-with open(csv_file, newline='', encoding='utf-8') as file:
+with open(csv_op, newline='', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
         if row['Nom'] not in row['Alias']:
@@ -30,11 +32,34 @@ with open(csv_file, newline='', encoding='utf-8') as file:
         }
         characters.append(character)
 
+with open(csv_naruto, newline='', encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        # Créer un dictionnaire pour chaque personnage de Naruto
+        character = {
+            'manga': row['Manga'],
+            'nom': row['Nom'],
+            'alias': row['Alias'].split(', '),
+            'genre': row['Genre'],
+            'espece': row['Espèce'],
+            'nature_chakra': row['Nature de Chakra'],
+            'vivant': row['Vivant'],
+            'clan': row['Clan'],
+            'pouvoir': row['Pouvoir héréditaire'],
+            'affiliation': row['Affiliation'],
+            'arc': row['Arc d\'Apparition'],
+            'imgpath': row['imgpath']
+        }
+        n_characters.append(character)
 
 
 # Générer le code JavaScript
 js_code = "export const characters = " + json.dumps(characters, indent=4) + ";"
+naruto_js_code = "export const characters = " + json.dumps(n_characters, indent=4) + ";"
 
 # Écrire le code JavaScript dans un fichier
 with open('./src/op/char.js', 'w') as js_file:
     js_file.write(js_code)
+
+with open('./src/naruto/char.js', 'w') as naruto_js_file:
+    naruto_js_file.write(naruto_js_code)
